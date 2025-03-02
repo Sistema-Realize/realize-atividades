0f890ebe63cc4e3f6e43e689c75b37559b2e8ed2
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2025-03-02 11:07:24 -0300

    Fix -  Ajustado o URL de redirecionamento para o form

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -4,7 +4,7 @@ import { config } from "../../../config/environment";
 export default function login(req: NextApiRequest, res: NextApiResponse) {
   const auth0Domain = config.AUTH0_DOMAIN;
   const clientId = config.AUTH0_CLIENT_ID;
-  const redirectUri = `${config.AUTH0_BASE_URL}/api/auth/callback`;
+  const redirectUri = `${config.AUTH0_BASE_URL}/pages/form`;
 
   const authUrl = `${auth0Domain}/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=openid profile email`;
 
