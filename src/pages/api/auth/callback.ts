28535e96dffa9304bf966f78bdebcbd970af7f2b
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2025-03-02 10:36:25 -0300

    Ajustando as variaveis para login

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -8,33 +8,37 @@ export default async function callback(
 ): Promise<void> {
   const { code } = req.query as { code: string };
 
+  if (!code) {
+    return res.status(400).json({ error: "Código de autorização ausente" });
+  }
+
   const clientId = config.AUTH0_CLIENT_ID;
   const clientSecret = config.AUTH0_CLIENT_SECRET;
-  const redirectUri = `${config.AUTH0_BASE_URL}/api/auth/callback`;
+  const redirectUri = `${config.NEXT_PUBLIC_BASE_URL}/api/auth/callback`;
 
   try {
-    // Trocar o código de autorização pelos tokens
-    const response = await axios.post(`${config.AUTH0_BASE_URL}/oauth/token`, {
-      grant_type: "authorization_code",
-      client_id: clientId,
-      client_secret: clientSecret,
-      code,
-      redirect_uri: redirectUri,
-    });
+    // Troca o código de autorização pelos tokens
+    const response = await axios.post(
+      `https://${config.AUTH0_DOMAIN}/oauth/token`,
+      {
+        grant_type: "authorization_code",
+        client_id: clientId,
+        client_secret: clientSecret,
+        code,
+        redirect_uri: redirectUri,
+      }
+    );
 
     const { id_token } = response.data;
 
-    // Armazenar o token em um cookie
+    // Armazena o token em um cookie seguro
     res.setHeader(
       "Set-Cookie",
-      `token=${id_token}; HttpOnly; Path=/; Max-Age=3600`
+      `token=${id_token}; HttpOnly; Secure; SameSite=Lax; Path=/; Max-Age=3600`
     );
 
-    // Redirecionamento baseado no ambiente
-    const redirectURL =
-      process.env.NODE_ENV === "production"
-        ? "https://atividades.realize.pro.br/form"
-        : "http://localhost:3000/form";
+    // Define a URL de redirecionamento corretamente
+    const redirectURL = `${config.NEXT_PUBLIC_BASE_URL}/form`;
 
     res.redirect(redirectURL);
   } catch (error) {
