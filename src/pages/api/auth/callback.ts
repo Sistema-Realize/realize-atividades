43feb64552ff01380258a74f1bc2b45c37cdb047
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2025-03-02 10:29:36 -0300

    Ajustando o redirecinamento do login

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -8,13 +8,13 @@ export default async function callback(
 ): Promise<void> {
   const { code } = req.query as { code: string };
 
-  const clientId = config.AUTH0_CLIENT_ID as string;
-  const clientSecret = config.AUTH0_CLIENT_SECRET as string;
-  const redirectUri = `${config.AUTH0_BASE_URL}/api/auth/callback` as string;
+  const clientId = config.AUTH0_CLIENT_ID;
+  const clientSecret = config.AUTH0_CLIENT_SECRET;
+  const redirectUri = `${config.AUTH0_BASE_URL}/api/auth/callback`;
 
   try {
-    // Exchange the authorization code for tokens
-    const response = await axios.post(`${config.AUTH0_DOMAIN}/oauth/token`, {
+    // Trocar o código de autorização pelos tokens
+    const response = await axios.post(`${config.AUTH0_BASE_URL}/oauth/token`, {
       grant_type: "authorization_code",
       client_id: clientId,
       client_secret: clientSecret,
@@ -24,16 +24,21 @@ export default async function callback(
 
     const { id_token } = response.data;
 
-    // Store the token in a cookie or session
+    // Armazenar o token em um cookie
     res.setHeader(
       "Set-Cookie",
       `token=${id_token}; HttpOnly; Path=/; Max-Age=3600`
     );
 
-    // Redirect to the form page instead of the dashboard
-    res.redirect("/form");
+    // Redirecionamento baseado no ambiente
+    const redirectURL =
+      process.env.NODE_ENV === "production"
+        ? "https://atividades.realize.pro.br/form"
+        : "http://localhost:3000/form";
+
+    res.redirect(redirectURL);
   } catch (error) {
-    console.error("Error during authentication:", error);
-    res.status(500).json({ error: "Authentication failed" });
+    console.error("Erro durante a autenticação:", error);
+    res.status(500).json({ error: "Falha na autenticação" });
   }
 }
