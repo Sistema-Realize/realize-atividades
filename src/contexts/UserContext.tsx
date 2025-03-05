56752a8b3eba657c52473ae4fd97fc1a69dd681a
Gustavo Lima <gustavomdcl@gmail.com>
2025-03-05 00:44:05 -0300

    feat: create subscription call

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,9 +1,10 @@
+import { createContext, useContext, useEffect, useState } from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
-import { createContext, useContext } from 'react';
 
 interface UserContextType {
   userId: string;
   isLoggedIn: boolean;
+  isSubscriptionActive: boolean;
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
@@ -12,9 +13,22 @@ export function UserContextProvider({ children }: { children: React.ReactNode })
   const { user } = useUser();
   const userId = user?.sub as string ?? "";
   const isLoggedIn = !!userId;
+  const [isSubscriptionActive, setIsSubscriptionActive] = useState(false);
+
+  useEffect(() => {
+    const fetchSubscriptionStatus = async () => {
+      if (!isLoggedIn) return;
+      const response = await fetch(`/api/subscription`, {
+        method: 'GET'
+      });
+      const data = await response.json();
+      setIsSubscriptionActive(data.isActive);
+    };
+    fetchSubscriptionStatus();
+  }, [isLoggedIn, userId]);
 
   return (
-    <UserContext.Provider value={{ userId, isLoggedIn }}>
+    <UserContext.Provider value={{ userId, isLoggedIn, isSubscriptionActive }}>
       {children}
     </UserContext.Provider>
   );
